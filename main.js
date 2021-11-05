a219532182ef59ef638cd37c868889d4ef0fea35
MyUsername <my@email.com>
2021-11-05 17:57:24 +0100

    commit

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let post = [
         "fotoProfilo" : "https://unsplash.it/300/300?image=",
         "data" : "8 mesi fa",
         "testoPost" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "immagine" : "https://unsplash.it/300/300?image=",
+        "immagine" : "",
         "numeroLike" : "100"
     },
 
@@ -32,42 +32,55 @@ let post = [
     }
 ]
 
-for (let i = 0; i < post.length; i++) {
-    const {nomeAutote, fotoProfilo, data, testoPost, immagine, numeroLike} = post[i];
-    document.getElementById("container").innerHTML +=
-            `<div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${fotoProfilo}">                    
+function stampaPost() {
+    for (let i = 0; i < post.length; i++) {
+        const {nomeAutote, fotoProfilo, data, testoPost, immagine, numeroLike} = post[i];
+        document.getElementById("container").innerHTML +=
+                `<div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${fotoProfilo}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${nomeAutote}</div>
+                            <div class="post-meta__time">${data}</div>
+                        </div>                    
                     </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${nomeAutote}</div>
-                        <div class="post-meta__time">${data}</div>
-                    </div>                    
                 </div>
-            </div>
-            <div class="post__text">${testoPost}</div>
-            <div class="post__image">
-                <img src="${immagine}">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" onClick="incrementa()" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${numeroLike}</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div>`;
+                <div class="post__text">${testoPost}</div>
+                <div class="post__image">
+                    <img src="${immagine}">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="${i}">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${numeroLike}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div>`;
+    }
+}
 
-        function incrementa(){
-            var i = document.getElementById("like-counter-1");
-            i.innerHTML = Math.min(81,+i.innerHTML+1);
-            }
+stampaPost();
+
+let buttons = document.querySelectorAll(".js-like-button");
+let numeroLike = document.querySelectorAll(".js-likes-counter");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(event) {
+        event.stopPropagation();
+
+        const index = this.getAttribute("data-postid");
+        post[index].numeroLike = parseInt(post[index].numeroLike) + 1;
+
+        stampaPost();
+    });
 }
