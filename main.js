53c3613ebdc9f6aa1cfc74d7bd207386fb579754
MyUsername <my@email.com>
2021-11-05 14:57:16 +0100

    inizio

---
+++ main.js
@@ -0,0 +1,42 @@
+// 1. creo un array di oggetti che rappresentino i vari post 
+// 2. inseriamo negli oggetti le caratteristiche dei vari post 
+// 3. stampiamo nell'html le varie card dei post 
+// 4. rendo il tasto like cliccabile e faccio si che incrementi i like del post di 1
+
+let post = [
+    {
+        "nomeAutote" : "",
+        "fotoProfilo" : "",
+        "data" : "bho",
+        "testoPost" : "",
+        "immagine" : "",
+        "numeroLike" : ""
+    },
+
+    {
+        "nomeAutote" : "",
+        "fotoProfilo" : "",
+        "data" : "bho",
+        "testoPost" : "",
+        "immagine" : "",
+        "numeroLike" : ""
+    },
+
+    {
+        "nomeAutote" : "",
+        "fotoProfilo" : "",
+        "data" : "bho",
+        "testoPost" : "",
+        "immagine" : "",
+        "numeroLike" : ""
+    },
+
+    {
+        "nomeAutote" : "",
+        "fotoProfilo" : "",
+        "data" : "bho",
+        "testoPost" : "",
+        "immagine" : "",
+        "numeroLike" : ""
+    }
+]
